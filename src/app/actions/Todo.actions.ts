29258120964b70c234bc1fbe3fca82a6a1d3adbf
Ryan Nickel <ryan@ryannickel.com>
2024-03-19 15:59:13 -0400

    Auto-generated commit

--- src/app/actions/Todo.actions.ts
+++
@@ -1,9 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Todo } from '../models/Todo';
-
-export const add = createAction('[Todo Component] add', props<{ todo: string }>());
-export const todoCreated = createAction('[Todo Component todo created', props<{ todo: Todo }>())
-export const toggle = createAction('[Todo Component] toggle', props<{ id: number }>());
-export const todoToggled = createAction('[Todo toggled] todo toglled', props<{ todo: Todo }>())
-export const setTodos = createAction('[Todo Component] setTodos', props<{ todos: Todo[] }>());
-export const loadTodos = createAction('[Todo Component] loadTodos');
